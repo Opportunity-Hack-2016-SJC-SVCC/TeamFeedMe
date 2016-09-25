006b61fbd6fa77fcdaa852fb0a7b75768747008e
mdhussain <mohd.hussain@gmail.com>
2016-09-25 08:57:56 -0700

    Visual styling and API integration with maps.

--- visual/js/index.js
+++ visual/js/index.js
@@ -21,54 +21,74 @@ function initMap() {
 
         var i;
         infoWindow = new google.maps.InfoWindow();
-
-        getRequestData();
-        //getMealData();
     } catch (e) {
         alert('An error has occurred: ' + e.message);
     }
 }
 
 
-function getRequestData()
+function showRequestData(requests)
 {
-    try{
-    var sampleResponse = '{"requests":[{"count":"3","created_on":"1474749729317","id":"1","location":{"lat":"37.4","lng":"-121.92"},"status":"completed","updated_on":"1474749729317"},{"count":"5","created_on":"1474749729317","id":"2","location":{"lat":"37.3769713","lng":"-121.9227678"},"status":"pending","updated_on":"1474749729317"},{"count":"5","created_on":"1474749729317","id":"2","location":{"lat":"37.403","lng":"-121.9304"},"status":"pending","updated_on":"1474749729317"}]}';
-    var jsonObject = JSON.parse(sampleResponse);
-    var length = Object.keys(jsonObject.requests).length;
-    var i =0;
-    for(i =0;i<length;i++)
-    {
-        if(jsonObject.requests[i].status == 'pending')
-            {
-        addMarkersOnMap(jsonObject.requests[i].location.lat, jsonObject.requests[i].location.lng,'Requestor', "Count="+jsonObject.requests[i].count+" Status="+jsonObject.requests[i].status, jsonObject.requests[i].count, 'FE7569');
-            }
-        else
-            {
-        addMarkersOnMap(jsonObject.requests[i].location.lat, jsonObject.requests[i].location.lng,'Requestor', "Count="+jsonObject.requests[i].count+" Status="+jsonObject.requests[i].status, jsonObject.requests[i].count, 'AAAAAA');
-            }
+    try {
+      if(requests) {
+          var jsonObject = requests;
+          var length = jsonObject.length;
+          var i =0;
+          for(i =0;i<length;i++) {
+              if(jsonObject[i].status == 'pending'){
+                addMarkersOnMap(jsonObject[i].location.lat, jsonObject[i].location.lng,'Requestor', "Count="+jsonObject[i].count+" Status="+jsonObject[i].status, jsonObject[i].count, 'FE7569');
+              }
+              else{
+                addMarkersOnMap(jsonObject[i].location.lat, jsonObject[i].location.lng,'Requestor', "Count="+jsonObject[i].count+" Status="+jsonObject[i].status, jsonObject[i].count, 'AAAAAA');
+              }
+          }
+      }
     }
+    catch(e) {
+      alert('An error has occurred 1 : ' + e.message);
     }
-    catch(e)
-    {alert('An error has occurred: ' + e.message);}
 }
 
-function showMealData(data)
+
+function showMealData(mealData)
 {
-    try{
-    var text = data;
-    alert(JSON.stringify(data));
-    var sampleResponse = '{"meals":[{"count":"3","created_on":"1474749729317","expiry":"1474784598","location":{"lat":"37.279518","lng":"-121.867905"},"type":"vegan","updated_on":"1474749729317"},{"count":"5","created_on":"1474749729317","expiry":"1474784598","location":{"lat":"37.41118","lng":"-121.927391"},"type":"vegetarian","updated_on":"1474749729317"},{"count":"5","created_on":"1474749729317","expiry":"1474784598","location":{"lat":"37.323","lng":"-122.0527"},"type":"non-vegetarian","updated_on":"1474749729317"}]}';
-    var jsonObject = JSON.parse(sampleResponse);
-    var length = Object.keys(jsonObject.meals).length;
-    var i =0;
-    for(i =0;i<length;i++)
-    {
-        addMarkersOnMap(jsonObject.meals[i].location.lat, jsonObject.meals[i].location.lng,'Donator', "Count="+jsonObject.meals[i].count+" Type="+jsonObject.meals[i].type+" Expiry Date="+getDate(jsonObject.meals[i].expiry), jsonObject.meals[i].count, 'FFF000');
+    try {
+      if(mealData) {
+          var jsonObject = mealData;
+          var length = jsonObject.length;
+          var i =0;
+          for(i =0;i<length;i++){
+              addMarkersOnMap(jsonObject[i].location.lat, jsonObject[i].location.lng,'Meals', getTypeString(jsonObject[i].type) + " " + getMealText(jsonObject[i].count) + " available.", jsonObject[i].count, 'FFF000');
+          }
+      }
     }
+    catch(e){
+      alert('An error has occurred 2 : ' + e.message);
     }
-    catch(e)
-    {alert('An error has occurred: ' + e.message);}
+}
+
+function getTypeString(type){
+    switch(type) {
+        case "vegetarian":
+            return "Vegetarian";
+            break;
+        case "non-vegetarian":
+            return "Non-vegetarian";
+            break;
+        case "vegan":
+            return "Vegan";
+            break;
+        case "halal":
+            return "Halal";
+            break;
+        case "any":
+        default:
+            return "Any";
+    }
+}
+
+function getMealText(count){
+    return count > 1 ? "meals":"meal";
 }
 
 function addMarkersOnMap(latitude, longitude, title, description, count, color)
